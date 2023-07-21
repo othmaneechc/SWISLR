4fbe5034e19624c94b3ef272f57013198cac3abf
Nathan Yang <45131167+Knicey@users.noreply.github.com>
2023-07-20 20:03:39 -0400

    Updated Version of Script + New CSVs
    I updated the script to now search through the different location data individually if nothing is found when they are combined.
    The script now also correctly accounts for lack of location data and when a location isn't found from a search.

--- geolocator/AddressToLatLong.py
+++ geolocator/AddressToLatLong.py
@@ -6,44 +6,67 @@ geolocator = Nominatim(user_agent="MyApp")
 #location = geolocator.geocode("North Carolina")
 
 ###Read the Data
-path = "Study_Sites_Cleaned.csv"
+path = "10_New_Sites"
 dictList = []
-with open(path, newline='') as csvfile:
+print("\nDo not include redundant location information, it will lower the accuracy of Nominatim\n")
+with open(path + ".csv", newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     fieldnames = reader.fieldnames
 
-    ###Finding which fields are present and making them all lowercase
-    addressFields = ["country", "state", "county", "city", "neighborhood", "addresss"]
+    ###Lowercasing all the fields and finding which fields are present
+    addressFields = ["location", "addresss", "city", "county", "state", "country"]
     detectedFields = []
     for i in range(len(fieldnames)):
         fieldnames[i] = fieldnames[i].lower()
-        if fieldnames[i] in addressFields:
-            detectedFields.append(fieldnames[i])
+    for field in addressFields:
+        if (field in fieldnames):
+            detectedFields.append(field)
+            if (len(detectedFields) > 3):
+                break
+    numDetected = len(detectedFields)
     print("Fieldnames", fieldnames)
-    print("We found these address fields", detectedFields)
     
-
+    if numDetected == 0:
+        print("We did not find any address fields, please make sure your data is labeled appropriately")
+        exit()
+    print("We found these address fields", detectedFields)
+    numDetected = len(detectedFields)
+    
     ###Update all the data with the new fields
-    fieldnames += ['Latitude', 'Longitude']
+    fieldnames += ['latitude', 'longitude']
     for row in reader:  
-        searchQuery = ''
+        searchQuery = []
         for field in detectedFields:
-            searchQuery += row[field]
-        print("Searching Nominatim for", searchQuery)
-        location = geolocator.geocode(searchQuery)
-        row['Latitude'] = location.latitude
-        row['Longitude'] = location.longitude
+            if row[field] != "":
+                searchQuery.append(row[field])
+        print("Search Terms", searchQuery)
+        print("\nSearching Nominatim for", " ".join(searchQuery))
+        location = geolocator.geocode(" ".join(searchQuery))
+
+        ###if no results, try first location only
+        if location == None:
+            print("Location not found")
+            print("Instead searching for", searchQuery[0])
+            location = geolocator.geocode(searchQuery[0])
+
+        ###Searching for second location
+        if (location == None)  and (numDetected > 1):
+            print("Location not found")
+            print("Instead searching for", searchQuery[1])
+            location = geolocator.geocode(searchQuery[1])
+
+        if location == None:
+            print("Location not found\nPlease double check location data")
+            continue
+
+        row['latitude'] = location.latitude
+        row['longitude'] = location.longitude
         dictList.append(row)  
-print(dictList)
+#print(dictList)
 
 ###Write the Data to a new spreadsheet
-outpath = "Study_Sites_LatLong.csv"
-with open(outpath, 'w', newline='') as csvfile:
+with open(path + "_LatLong.csv", 'w', newline='') as csvfile:
     headers = fieldnames
     writer = csv.DictWriter(csvfile, fieldnames= headers)
     writer.writeheader()
     writer.writerows(dictList)
-
-
-    
-
